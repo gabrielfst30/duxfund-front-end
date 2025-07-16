0be15e19002a4416a5f8712248d74521ffad462c
gabrielfst30 <gabrielfst30@gmail.com>
2025-07-16 00:57:58 -0300

    refactor: refatorando estrutura html

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,17 +3,12 @@
 import Image from "next/image";
 import styles from "./styles.module.scss";
 import { useState } from 'react';
-import { connectClient } from "@/xrpl/xrplClient";
 
 export default function Home() {
 
-  const handleCheck = async () => {
-    const result = await connectClient()
-    console.log(result)
-  }
   return (
     <div>
-      <button onClick={handleCheck}>TESTE</button>
+      <button></button>
     </div>
   );
 }
