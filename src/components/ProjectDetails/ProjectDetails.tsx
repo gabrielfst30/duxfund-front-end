9f80a8e43e2a625b54416018a7b3fb0c531125fc
Rafael Motta de Freitas <105321718+RafaelMDF@users.noreply.github.com>
2025-07-20 14:45:57 -0300

    fix: estilização do componente projectdetails

--- src/components/ProjectDetails/ProjectDetails.tsx
+++ src/components/ProjectDetails/ProjectDetails.tsx
@@ -3,14 +3,19 @@ import styles from "./styles.module.scss";
 export function ProjectDetails() {
   return (
     <div className={styles.ProjectDetails}>
-      <h1>Project Details</h1>
-      <p>Fund the Future of Web3</p>
-      <p>
-        Help young blockchain developers transform the crypto ecosystem with
-        DuxFund, an innovative project that connects purpose, technology, and
-        social impact. Your XRP donation, of any amount, fuels the development
-        of real, decentralized solutions.
-      </p>
+      <div className={styles.title}>
+        <h1>Project Details</h1>
+        <div className={styles.subtitle}>Fund the Future of Web3</div>
+      </div>
+
+      <div className={styles.text}>
+        <p>
+          Help young blockchain developers transform the crypto ecosystem with
+          DuxFund, an innovative project that connects purpose, technology, and
+          social impact. Your XRP donation, of any amount, fuels the development
+          of real, decentralized solutions.
+        </p>
+      </div>
     </div>
   );
 }
