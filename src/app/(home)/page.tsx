e49d93c021fcd6cc257420397caf0cbd42c4b3d4
Rafael Motta de Freitas <105321718+RafaelMDF@users.noreply.github.com>
2025-07-20 14:15:56 -0300

    fix: estilização do componente home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,28 +1,29 @@
-import Image from "next/image";
 import styles from "./styles.module.scss";
 import { Contribute } from "@/components/Contribute/Contribute";
 import { ProjectDetails } from "@/components/ProjectDetails/ProjectDetails";
 import { Lastest } from "@/components/Lastest/Lastest";
 import { Navbar } from "@/components/Navbar/Navbar";
 
-
 export default function Home() {
   return (
-    <div>
-      <div>
+    <div className={styles.pageContainer}>
+      <div className={styles.header}>
         <Navbar />
       </div>
 
-      <div>
-        <Contribute />
-      </div>
+      <div className={styles.content}>
+        <div className={styles.leftContent}>
+          <div>
+            <Contribute />
+          </div>
+          <div>
+            <ProjectDetails />
+          </div>
+        </div>
 
-      <div>
-        <ProjectDetails />
-      </div>
-
-      <div>
-        <Lastest />
+        <div className={styles.rightContent}>
+          <Lastest />
+        </div>
       </div>
     </div>
   );
