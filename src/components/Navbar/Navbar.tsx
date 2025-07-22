c28a05a7827f4cf7c7ff5718842b937907a13339
Rafael Motta de Freitas <105321718+RafaelMDF@users.noreply.github.com>
2025-07-22 03:09:13 -0300

    feat: implementação da logo ao lado do nome

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,10 @@ export function Navbar() {
   return (
     <div className={styles.Navbar}>
       <div className={styles.logo}>
-        <h1>DuxFund</h1>
+        <h1>
+          <img src="/logoduxfund.png" alt="Logo" className={styles.logoImage} />
+          DuxFund
+        </h1>
       </div>
       <div className={styles.connect}>
         <p>Connect Wallet</p>
