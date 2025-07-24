72ece93338ad9694f0ff1a9b7ea15b5bc96ba02c
gabrielfst30 <gabrielfst30@gmail.com>
2025-07-24 00:46:27 -0300

    feature: criando tipagem das transações baseadas na modelagem do banco de dados

---
+++ src/types/transaction.ts
@@ -0,0 +1,36 @@
+// Tipos baseados no DER fornecido
+
+export interface Transaction {
+  id: number;
+  id_payments: number;
+  payments: Payment
+  id_nft_mints: number;
+  nft_mints: NftMints
+  xrpl_id: number;
+  type: string;
+  hash: string;
+  ledger_index: number;
+  close_time: string; // ISO string para timestamp
+  validated: boolean;
+  result_code: string;
+  fee: BigInt;
+  account: string;
+  sequence: BigInt;
+  last_ledger_seq?: BigInt;
+  flags?: BigInt;
+  ledger_hash?: string;
+  ctid?: string;
+}
+
+export interface Payment {
+  id: number;
+  destinations: string;
+  deliver_max?: number;
+  delivered_amount?: string;
+}
+
+export interface NftMints {
+  id: number;
+  uri: string;
+  taxon?: number;
+}
