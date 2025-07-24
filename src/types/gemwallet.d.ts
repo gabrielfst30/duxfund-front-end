7b1b56e8aee5dd3229055e91c6d54d18941e39ef
gabrielfst30 <gabrielfst30@gmail.com>
2025-07-23 23:58:53 -0300

    excluindo arquivo irrelevante

--- src/types/gemwallet.d.ts
+++
@@ -1,7 +0,0 @@
-export {};
-
-declare global { // definindo a interface da gem wallet
-  interface Window { // definindo a interface da gem wallet
-    gemWallet?: any; // gemwallet pode existir ou não
-  }
-}
