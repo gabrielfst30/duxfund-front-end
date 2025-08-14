15dcd7783e312bf3dcb99a32d5f7f8d783f3dd70
gabrielfst30 <gabrielfst30@gmail.com>
2025-08-14 02:47:50 -0300

    fix: removendo parametros desnecessários

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,24 +1,12 @@
 "use client";
-
-import { useState } from "react";
 import styles from "./styles.module.scss";
 import { Contribute } from "@/components/Contribute/Contribute";
-import { Lastest } from "@/components/Lastest/Lastest";
+import { LatestContributions } from "@/components/LatestContributions/LatestContributions";
 import { ProjectDetails } from "@/components/ProjectDetails/ProjectDetails";
 import { Navbar } from "@/components/Navbar/Navbar";
 
 
 export default function Home() {
-  const [contributions, setContributions] = useState<Contribution[]>([]);
-
-  // function handleSendContribution(amount: string) {
-  //   const newContribution = {
-  //     amount: Number(amount), // Convertendo string para number
-  //     timestamp: new Date().toISOString(), // gera data atual em ISO
-  //   };
-  //   setContributions((prev) => [newContribution, ...prev]); // adiciona nova contribuição no topo
-  // }
-
   return (
     <div className={styles.pageContainer}>
       <div className={styles.header}>
@@ -27,12 +15,12 @@ export default function Home() {
 
       <div className={styles.content}>
         <div className={styles.leftContent}>
-          <Contribute/>
+          <Contribute />
           <ProjectDetails />
         </div>
 
         <div className={styles.rightContent}>
-          <Lastest contributions={contributions} />
+          <LatestContributions />
         </div>
       </div>
     </div>
