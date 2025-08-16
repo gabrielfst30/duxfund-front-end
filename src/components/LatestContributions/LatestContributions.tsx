7fa4b537bef858071d031ceaba1ec7f6b1dee61f
Victor-Rocha77 <victorrochavieira@gmail.com>
2025-08-15 23:30:52 -0300

    feature: adicionando componente Navbar com integração GemWallet

--- src/components/LatestContributions/LatestContributions.tsx
+++ src/components/LatestContributions/LatestContributions.tsx
@@ -14,103 +14,89 @@ interface Contribution {
 }
 
 export function LatestContributions() {
-  const [contributions, setContributions] = useState<Contribution[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
+  // const [contributions, setContributions] = useState<Contribution[]>([]);
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState<string | null>(null);
 
-  const getContributions = async () => {
-    try {
-      setLoading(true);
-      const response = await fetch('/api/show_all_nfts', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        }
-      });
+  // const getContributions = async () => {
+  //   try {
+  //     setLoading(true);
+  //     const response = await fetch('/api/show_all_nfts', {
+  //       method: 'GET',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       }
+  //     });
 
-      if (!response.ok) {
-        throw new Error('Falha ao carregar contribuições');
-      }
+  //     if (!response.ok) {
+  //       throw new Error('Falha ao carregar contribuições');
+  //     }
 
-      const responseResult = await response.json();
-      
-      if (responseResult && Array.isArray(responseResult)) {
-        setContributions(responseResult);
-      } else {
-        setContributions([]);
-      }
-    } catch (err) {
-      console.error('Erro ao carregar contribuições:', err);
-      setError(err instanceof Error ? err.message : 'Erro desconhecido');
-      setContributions([]);
-    } finally {
-      setLoading(false);
-    }
-  }
+  //     const responseResult = await response.json();
+
+  //     if (responseResult && Array.isArray(responseResult)) {
+  //       setContributions(responseResult);
+  //     } else {
+  //       setContributions([]);
+  //     }
+  //   } catch (err) {
+  //     console.error('Erro ao carregar contribuições:', err);
+  //     setError(err instanceof Error ? err.message : 'Erro desconhecido');
+  //     setContributions([]);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // }
 
 
-  console.log(contributions)
+  // console.log(contributions)
 
-  useEffect(() => {
-    getContributions()
-  }, [])
+  // useEffect(() => {
+  //   getContributions()
+  // }, [])
 
-  if (loading) {
-    return (
-      <div className={styles.Lastest}>
-        <h1>Latest Contributions (LP Token)</h1>
-        <div className={styles.list}>
-          <div className={styles.card}>
-            <div className={styles.left}>
-              <span className={styles.address}>Carregando...</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <div className={styles.Lastest}>
+  //       <h1>Latest Contributions (LP Token)</h1>
+  //       <div className={styles.list}>
+  //         <div className={styles.card}>
+  //           <div className={styles.left}>
+  //             <span className={styles.address}>Carregando...</span>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
-  if (error) {
-    return (
-      <div className={styles.Lastest}>
-        <h1>Latest Contributions (LP Token)</h1>
-        <div className={styles.list}>
-          <div className={styles.card}>
-            <div className={styles.left}>
-              <span className={styles.address}>Erro: {error}</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+  // if (error) {
+  //   return (
+  //     <div className={styles.Lastest}>
+  //       <h1>Latest Contributions (LP Token)</h1>
+  //       <div className={styles.list}>
+  //         <div className={styles.card}>
+  //           <div className={styles.left}>
+  //             <span className={styles.address}>Erro: {error}</span>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className={styles.Lastest}>
       <h1>Latest Contributions (LP Token)</h1>
       <div className={styles.list}>
-        {(!contributions || contributions.length === 0) ? (
-          <div className={styles.card}>
-            <div className={styles.left}>
-              <span className={styles.address}>No contributions yet.</span>
-            </div>
+        <div className={styles.card}>
+          <div className={styles.left}>
+            <span className={styles.address}>No contributions yet.</span>
           </div>
-        ) : (
-          contributions.map((contribution, index) => {
-            return (
-              <div className={styles.card} key={index}>
-                <div className={styles.left}>
-                  <span className={styles.address}>Payment Tx: {contribution.payment_hash}</span>
-                  <span className={styles.time}>
-                    {contribution.created_at}
-                  </span>
-                </div>
-                <div className={styles.right}>{contribution.nft_hash} XRP</div>
-              </div>
-            );
-          })
-        )}
+        </div>
       </div>
     </div>
+
+
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,109 +2,140 @@
 import Image from "next/image";
 import styles from "./styles.module.scss";
 import { useState, useEffect } from 'react'
+import { isInstalled, getAddress } from "@gemwallet/api";
+
 
 export function Navbar() {
   const [account, setAccount] = useState('') // Endereço da carteira
   const [payloadUuid, setPayloadUuid] = useState('') // UUID gerado pela API do XUMM
   const [isConnecting, setIsConnecting] = useState(false) // Loading de conexão
   const [connectUrl, setConnectUrl] = useState('') // URL gerada para assinar o payload
+  const [name, setName] = useState("Victor")
+  const [error, setError] = useState('')
 
-  // Função para criar um novo payload (request) de login
-  const createSignInPayload = async () => {
+  const handleConnectWallet = async () => {
+    setError("");
+    setIsConnecting(true);
     try {
-      setIsConnecting(true)
-      const response = await fetch('/api/xumm_wallet', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({}) // Sem UUID = criar novo payload
-      })
-
-      if (!response.ok) {
-        throw new Error('Falha ao criar payload')
+      const installed = await isInstalled();
+      if (!installed) {
+        setError("GemWallet is not installed")
+        setIsConnecting(false);
+        return;
       }
-
-      // Resposta do payload
-      const data = await response.json()
-      setPayloadUuid(data.uuid) // Setando UUID do payload (assinatura)
-      setConnectUrl(data.connectUrl) // URL para conectar
-      
-      // Abrir a URL da Xaman para conexão
-      window.open(data.connectUrl, '_blank') // Abrir a URL da Xaman
-      
-      // Iniciar polling (envia o uuid gerado)
-      startPolling(data.uuid)
-    } catch (error) {
-      console.error('Erro ao conectar:', error)
-      setIsConnecting(false)
+      const addressResponse = await getAddress();
+      if (addressResponse && addressResponse.address) {
+        setAccount(addressResponse.address);
+      } else {
+        setError("Failed to get address from GemWallet")
+      }
+    } catch (err) {
+      setError("Failed to connect to GemWallet");
     }
-  }
+    setIsConnecting(false);
+  };
 
-  // Função para fazer polling do status do payload 
-  const startPolling = (uuid: string) => {
 
-    // (Envia um novo POST a cada 2 segundos passando o UUID do payload no body)
-    const pollInterval = setInterval(async () => {
-      try {
-        const response = await fetch('/api/xumm_wallet', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ uuid })
-        })
 
-        if (!response.ok) {
-          throw new Error('Falha ao verificar status')
-        }
 
-        const data = await response.json()
+
+
+// Função para criar um novo payload (request) de login
+  // const createSignInPayload = async () => {
+  //   try {
+  //     setIsConnecting(true)
+  //     const response = await fetch('/api/xumm_wallet', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify({}) // Sem UUID = criar novo payload
+  //     })
+
+  //     if (!response.ok) {
+  //       throw new Error('Falha ao criar payload')
+  //     }
+
+  //     // Resposta do payload
+  //     const data = await response.json()
+  //     setPayloadUuid(data.uuid) // Setando UUID do payload (assinatura)
+  //     setConnectUrl(data.connectUrl) // URL para conectar
+      
+  //     // Abrir a URL da Xaman para conexão
+  //     window.open(data.connectUrl, '_blank') // Abrir a URL da Xaman
+      
+  //     // Iniciar polling (envia o uuid gerado)
+  //     startPolling(data.uuid)
+  //   } catch (error) {
+  //     console.error('Erro ao conectar:', error)
+  //     setIsConnecting(false)
+  //   }
+  // }
+
+  // // Função para fazer polling do status do payload 
+  // const startPolling = (uuid: string) => {
+
+  //   // (Envia um novo POST a cada 2 segundos passando o UUID do payload no body)
+  //   const pollInterval = setInterval(async () => {
+  //     try {
+  //       const response = await fetch('/api/xumm_wallet', {
+  //         method: 'POST',
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //         },
+  //         body: JSON.stringify({ uuid })
+  //       })
+
+  //       if (!response.ok) {
+  //         throw new Error('Falha ao verificar status')
+  //       }
+
+  //       const data = await response.json()
         
-        //response
-        if (data.resolved) {
-          // Usuário autenticou com sucesso
-          setAccount(data.account)
-          setPayloadUuid('')
-          setConnectUrl('')
-          setIsConnecting(false)
-          clearInterval(pollInterval)
-          console.log('Conectado com sucesso:', data.account)
-        }
-      } catch (error) {
-        console.error('Erro no polling:', error)
-        setIsConnecting(false)
-        clearInterval(pollInterval)
-      }
-    }, 2000) // Verifica a cada 2 segundos
+  //       //response
+  //       if (data.resolved) {
+  //         // Usuário autenticou com sucesso
+  //         setAccount(data.account)
+  //         setPayloadUuid('')
+  //         setConnectUrl('')
+  //         setIsConnecting(false)
+  //         clearInterval(pollInterval)
+  //         console.log('Conectado com sucesso:', data.account)
+  //       }
+  //     } catch (error) {
+  //       console.error('Erro no polling:', error)
+  //       setIsConnecting(false)
+  //       clearInterval(pollInterval)
+  //     }
+  //   }, 2000) // Verifica a cada 2 segundos
 
-    // Parar polling após 5 minutos (timeout)
-    setTimeout(() => {
-      clearInterval(pollInterval)
-      setIsConnecting(false)
-      setPayloadUuid('')
-      setConnectUrl('')
-    }, 300000) // 5 minutos
-  }
+  //   // Parar polling após 5 minutos (timeout)
+  //   setTimeout(() => {
+  //     clearInterval(pollInterval)
+  //     setIsConnecting(false)
+  //     setPayloadUuid('')
+  //     setConnectUrl('')
+  //   }, 300000) // 5 minutos
+  // }
 
-  // Função para deslogar
-  const logout = () => {
-    setAccount('')
-    setPayloadUuid('')
-    setConnectUrl('')
-    setIsConnecting(false)
-  }
+  // // Função para deslogar
+  // const logout = () => {
+  //   setAccount('')
+  //   setPayloadUuid('')
+  //   setConnectUrl('')
+  //   setIsConnecting(false)
+  // }
 
-  // Função para conectar a wallet
-  const handleConnectWallet = () => {
-    if (account) {
-      // Se já está conectado, desconecta
-      logout()
-    } else if (!isConnecting) {
-      // Se não está conectando, inicia o processo
-      createSignInPayload()
-    }
-  }
+  // // Função para conectar a wallet
+  // const handleConnectWallet = () => {
+  //   if (account) {
+  //     // Se já está conectado, desconecta
+  //     logout()
+  //   } else if (!isConnecting) {
+  //     // Se não está conectando, inicia o processo
+  //     createSignInPayload()
+  //   }
+  // }
 
   return (
     <div className={styles.Navbar}>
@@ -117,19 +148,20 @@ export function Navbar() {
             height={40}
             className={styles.logoImage}
           />
-          DuxFund
+          DuxFund {name}
         </h1>
       </div>
 
-      <div className={styles.connect} onClick={handleConnectWallet}>
+      <div className={styles.connect}>
         <div className={styles.connectText}>
           {isConnecting ? (
             <p>Conectando...</p>
           ) : account ? (
             <p>Conectado: {account.slice(0, 6)}...{account.slice(-4)}</p>
           ) : (
-            <p>Connect Wallet</p>
+            <button onClick={handleConnectWallet}>Connect Wallet</button>
           )}
+            {error && <div style={{ color: "red" }}>{error}</div>}
         </div>
       </div>
     </div>
